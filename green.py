b0d9edda27553a238a57876fc9e024a8c864fa92
clumsycoder16 <ibrahimhakkikasim2@gmail.com>
2020-03-08 01:13:15 -0500

    green

---
+++ green.py
@@ -0,0 +1,20 @@
+import Cimpl
+def green_channel():
+    """ 
+    green_channel(image:Cimpl.Image) -> Cimpl.Image:
+    directs the user to choose an image that should be png, and 
+    changes the green colour of the every individual pixels to the highest limit
+    possible. 
+    
+    """
+    
+    image_file = Cimpl.choose_file()
+    original_image = Cimpl.load_image(image_file)
+    new_image = Cimpl.copy(original_image)
+    for pixel in original_image:
+        x,y,(r,g,b) = pixel
+        green_color = Cimpl.create_color(r,255,b)
+        Cimpl.set_color(new_image, x,y, green_color)
+        
+    Cimpl.show(new_image) 
+    

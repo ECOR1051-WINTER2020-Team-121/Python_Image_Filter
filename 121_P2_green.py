52b86ac5f78805058266dbdef448bbffc33a57e5
clumsycoder16 <ibrahimhakkikasim2@gmail.com>
2020-03-11 16:45:43 -0400

    final

--- 121_P2_green.py
+++ 121_P2_green.py
@@ -35,16 +35,32 @@ def check_equal(expected, outcome):
     and (x,y) coordinates of both images at which the images differ in pixel. 
 
     """
-    
+    test_increase = 0
     for x,y,(r,g,b) in var1:
         colour_tuple = Cimpl.get_color(var2,x,y)
-        if r != colour_tuple[0] and g != colour_tuple[1] and b != colour_tuple[2]:
-            print("The result of your comparison:")
-            return False
-    print("The result of your comparison:")
-    return True
-                
-green_channel()
-print(check_equal('green_image.png','riveter.jpg'))
+        if r != colour_tuple[0] or g != colour_tuple[1] or b != colour_tuple[2]:
+            test_increase += 1 
+            print("difference detected on the point ({},{}):".format(x,y))
+            print("The pixel of the expected image:({},{},{} vs. pixel of the outcome image:({},{},{}))".format(r,g,b,colour_tuple[0],colour_tuple[1],colour_tuple[2]))
 
-#test
+
+    if test_increase > 0:        
+        print("The result of your comparison:")
+        return False
+    else:
+        print("The result of your comparison:")
+        return True
+
+#Testing        
+green_channel()
+
+# It will return True as the two images match.
+print(check_equal('greenfiltered_.png','outcome.png'))
+
+
+# This example check will return False, since the expected image is original image while the outcome is filtered.
+# this was done to demonstrate how the test function works and it will print each points and the 
+# respective pixels that are every time the pixels are different. 
+print(check_equal('great_big_c.jpg','outcome.png'))
+
+

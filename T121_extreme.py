0b0eecdcff8a7022f5b3d23811872f171eb9acfe
clumsycoder16 <ibrahimhakkikasim2@gmail.com>
2020-03-14 01:49:35 -0400

    debugged extreme contrast

--- T121_extreme.py
+++ T121_extreme.py
@@ -11,12 +11,10 @@ def extreme_channel(original_image:Cimpl.Image)-> Cimpl.Image:
     new_image = Cimpl.copy(original_image)
     min_pixel = 0 
     max_pixel = 255 
-    for x,y,(r,g,b) in original_image:
-        colour_tuple = Cimpl.get_color(original_image,x,y)
-        
+    for x,y,colour_tuple in original_image:
+        i = 0 
         for component in colour_tuple:
             list_colour = list(colour_tuple)
-            i = 0 
             if component <= 127:
                 list_colour[i] = min_pixel
                 maximized_contrast = Cimpl.create_color(list_colour[0],list_colour[1],list_colour[2])
@@ -26,7 +24,7 @@ def extreme_channel(original_image:Cimpl.Image)-> Cimpl.Image:
                 maximized_contrast = Cimpl.create_color(list_colour[0],list_colour[1],list_colour[2])
                 Cimpl.set_color(new_image,x,y,maximized_contrast)
             i += 1 
-    Cimpl.save_as("outcome_extreme")    
+    #Cimpl.save_as(new_image,"outcome_extreme.png")    
     Cimpl.show(new_image)
     
         

fa2096d2b030575c98a27028339017bb6c3c8641
Zakaria Ismail <zisma3@ocdsb.ca>
2020-03-14 21:23:56 -0400

    P4-Task: Start of test function
    Start of two_tone and three_tone test function

--- P_4-Tasks/121_P4_posterize.py
+++ P_4-Tasks/121_P4_posterize.py
@@ -13,3 +13,23 @@ def posterize(img: Cimpl.Image) -> Cimpl.Image:
 
     (How do I write the examples?)
     """
+    img = Cimpl.copy(img)
+    for x, y, col in img:
+        channels = []
+        for ch in col:
+            channels += [__adjust_component__(ch)]
+        Cimpl.set_color(img, x, y, Cimpl.create_color(channels[0], channels[1], channels[1]))
+    return img
+
+
+def __adjust_component__(num: int) -> int:
+    """
+    RETURNS the midpoint of
+    one of the following ranges,
+    after being PASSED.
+    """
+    ranges = [63,127,191,255]
+    mid = [31, 95, 159, 223]
+    for i in range(len(ranges)):
+        if num <= ranges[i]:
+            return mid[i]

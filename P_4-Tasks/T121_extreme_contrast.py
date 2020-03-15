e99500b19b766e1b26b11a8f9871789a804c0e27
Zakaria Ismail <zisma3@ocdsb.ca>
2020-03-15 16:28:14 -0400

    P4 Tasks: extreme_contrast test function
    Test function for extreme_contrast

---
+++ P_4-Tasks/T121_extreme_contrast.py
@@ -0,0 +1,20 @@
+
+import Cimpl
+
+
+def extreme_contrast(img: Cimpl.Image) -> Cimpl.Image:
+    """
+    RETURNS a Cimpl.Image object
+    with pixels of extreme contrast,
+    after being PASSED a Cimpl.Image object.
+    """
+    copy = Cimpl.copy(img)
+    for x, y, col in img:
+        channels = []
+        for ch in col:
+            if ch < 128:
+                channels += [0]
+            else:
+                channels += [255]
+        Cimpl.set_color(copy, x, y, Cimpl.create_color(channels[0], channels[1], channels[2]))
+    return copy

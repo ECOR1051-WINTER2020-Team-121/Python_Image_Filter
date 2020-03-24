5edc392c647be730f968a488f3ffcc492961c44b
HimanshuSingh <himanshusingh12@yahoo.com>
2020-03-24 18:48:43 -0400

    Final

--- P5-task1/T121_P5_horizontal_test.py
+++ P5-task1/T121_P5_horizontal_test.py
@@ -69,7 +69,28 @@ def test_flip_horizontal():
     flip_horizontal_image = flip_horizontal(original)   
     for x, y, col in flip_horizontal_image: 
         check_equal('Checking pixel @(' + str(x) + ', ' + str(y) + ')',
-                     col, get_color(expected, x, y))    
+                     col, Cimpl.get_color(expected, x, y))    
+        
+        
+def flip_horizontal(img: Cimpl.Image) -> Cimpl.Image:
+    """
+    RETURNS a Cimpl.Image
+    that was flipped, after
+    being PASSED a Cimpl.Image object
+
+    >>> flip_horizontal(Cimpl.load_image('filename'))
+    """
+    hgt = Cimpl.get_height(img)
+    wth = Cimpl.get_width(img)
+    mid_x = wth // 2
+    copy = Cimpl.copy(img)
+    for y in range(hgt):
+        for x in range(mid_x):
+            #r1, g1, b1 = Cimpl.get_color(img, x, y)
+            #r2, g2, b2 = Cimpl.get_color(img, x, hgt-y)
+            Cimpl.set_color(copy, x, y, Cimpl.get_color(img, wth-x-1, y))
+            Cimpl.set_color(copy, wth-x-1, y, Cimpl.get_color(img, x, y))
+    return copy    
         
 #Main 
 test_flip_horizontal()

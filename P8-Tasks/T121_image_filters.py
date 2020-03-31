f2d97a37158678ee864b76ce3c0dc05920c888d3
Zakaria Ismail <zisma3@ocdsb.ca>
2020-03-31 14:32:16 -0400

    Added extreme_contrast function

--- P8-Tasks/T121_image_filters.py
+++ P8-Tasks/T121_image_filters.py
@@ -150,22 +150,31 @@ def detect_edges_better(img: Cimpl.Image, thres: int) -> Cimpl.Image:
     return copy
 
 
-def extreme_contrast(img: Cimpl.Image) -> Cimpl.Image:
+def extreme_contrast(original_image: Cimpl.Image) -> Cimpl.Image:
     """
-    RETURNS a Cimpl.Image object
-    with pixels of extreme contrast,
-    after being PASSED a Cimpl.Image object.
+    Author: Ibrahim Kasim
+    RETURNS a Cimpl.Imagee object where
+    the pixel channels in original_image have
+    been set to 0 or 255.
+
+    >>> extreme_contrast(Cimpl.load_image(choose_file()))
     """
-    copy = Cimpl.copy(img)
-    for x, y, col in img:
-        channels = []
-        for ch in col:
-            if ch < 128:
-                channels += [0]
-            else:
-                channels += [255]
-        Cimpl.set_color(copy, x, y, Cimpl.create_color(channels[0], channels[1], channels[2]))
-    return copy
+    new_image = Cimpl.copy(original_image)
+    min_pixel = 0
+    max_pixel = 255
+
+    for x, y, colour_tuple in original_image:
+        list_colour = []
+
+        for component in colour_tuple:
+            if component <= 127:
+                list_colour += [min_pixel]
+            elif component >= 128:
+                list_colour += [max_pixel]
+
+        maximized_contrast = Cimpl.create_color(list_colour[0], list_colour[1], list_colour[2])
+        Cimpl.set_color(new_image, x, y, maximized_contrast)
+    return new_image
 
 
 def flip_horizontal(img: Cimpl.Image) -> Cimpl.Image:

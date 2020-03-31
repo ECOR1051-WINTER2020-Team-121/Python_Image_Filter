a2f5fc20725836041fd2228d56b352e3d057311a
Zakaria Ismail <zisma3@ocdsb.ca>
2020-03-31 13:11:19 -0400

    Function extreme_contrast review
    Review notes of extreme_contrast can be found in Slack.

--- extremee.py
+++ extremee.py
@@ -1,32 +1,34 @@
 import Cimpl
+
+
 def extreme_contrast(original_image: Cimpl.Image) -> Cimpl.Image:
     """
-    The author: Ibrahim Kasim
-    returns image object with extreme contrast filter apllied.
-    
-        >>>test_image = Cimpl.load_image(Cimpl.choose_file())
-           extreme_channel(test_image)
-        ...(shows orginal image in extreme contrast filter)
+    Author: Ibrahim Kasim
+    RETURNS a Cimpl.Imagee object where
+    the pixel channels in original_image have
+    been set to 0 or 255.
 
+    >>> extreme_contrast(Cimpl.load_image(choose_file()))
     """
     new_image = Cimpl.copy(original_image)
     min_pixel = 0
     max_pixel = 255
     for x, y, colour_tuple in original_image:
-        i = 0
-        list_colour = [0,0,0]
-        for component in colour_tuple:
 
+        list_colour = []
+
+        for component in colour_tuple:
             if component <= 127:
-                list_colour[i] = min_pixel
-            
+                list_colour += [min_pixel]
             elif component >= 128:
-                list_colour[i] = max_pixel
-            i += 1
+                list_colour += [max_pixel]
+
         maximized_contrast = Cimpl.create_color(list_colour[0], list_colour[1], list_colour[2])
         Cimpl.set_color(new_image, x, y, maximized_contrast)
     
     return new_image
-    
+
+
+Cimpl.show(extreme_contrast(Cimpl.load_image('miss_sullivan.png')))
     
    
